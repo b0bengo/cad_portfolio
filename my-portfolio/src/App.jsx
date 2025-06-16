c5c2dbb5afd764cbbdaaa5f29778d0930ec093fb
b0bengo <jsobeng@outlook.com>
2025-06-16 23:16:17 +0100

    browser view

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './styles/main.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
